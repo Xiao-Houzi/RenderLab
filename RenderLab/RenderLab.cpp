77afd62bf8be46b36f53c83e78734397b9814c46
Xiao-Houzi <jasonsharp72@outlook.com>
2022-07-13 19:06:16 +0100

    Add project files.

---
+++ RenderLab/RenderLab.cpp
@@ -0,0 +1,12 @@
+﻿// RenderLab.cpp : Defines the entry point for the application.
+//
+
+#include "RenderLab.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
